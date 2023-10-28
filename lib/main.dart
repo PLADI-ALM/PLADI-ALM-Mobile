f4b995a3fb25af7d01b7c4c9b33ebe9dd883fd4a
ryr0121 <ryr0121@gmail.com>
2023-10-28 15:20:31 +0900

    config: 나눔스퀘어 폰트 추가

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         debugShowCheckedModeBanner: false, // 시뮬레이터 오른쪽 상단에 'debug' 배너 삭제
-        home: Scaffold(
+        theme: ThemeData(
+          primaryColor: Colors.white,
+          fontFamily: 'NanumSquare',
+        ),
+        home: const Scaffold(
           body: SplashScreen(),
         )
     );
