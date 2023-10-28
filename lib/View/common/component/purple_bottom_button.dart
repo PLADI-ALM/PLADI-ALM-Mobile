5e2f4704d37b55f9a94b52512ce614b4d1529b3b
ryr0121 <ryr0121@gmail.com>
2023-10-28 16:46:17 +0900

    config: sub app bar, 보라색 bottom 버튼 컴포넌트 일부 수정

--- lib/View/common/component/purple_bottom_button.dart
+++ lib/View/common/component/purple_bottom_button.dart
@@ -21,12 +21,16 @@ class _PurpleBottomButtonState extends State<PurpleBottomButton> {
   Widget build(BuildContext context) {
     TextStyle titleStyle = const TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20),
+      margin: const EdgeInsets.symmetric(horizontal: 18),
+      height: 44,
       child: ElevatedButton(
           onPressed: widget.onPressed,
           style: ElevatedButton.styleFrom(
             elevation: 0,
-            backgroundColor: purple
+            backgroundColor: purple,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            )
           ),
           child: Text(widget.title, style: titleStyle,)
       ),
