6f95666e3911f36a8bb8355ee815ac49e880a083
ryr0121 <ryr0121@gmail.com>
2023-10-28 23:41:19 +0900

    feat: 예약 메인 화면 상단 탭바 UI 구현

--- lib/View/booking/screen/booking_screen.dart
+++ lib/View/booking/screen/booking_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/View/colors.dart';
 import 'package:frontend/View/common/component/main_app_bar.dart';
 
 class BookingScreen extends StatefulWidget {
@@ -8,16 +9,82 @@ class BookingScreen extends StatefulWidget {
   State<BookingScreen> createState() => _BookingScreenState();
 }
 
-class _BookingScreenState extends State<BookingScreen> {
+class _BookingScreenState extends State<BookingScreen> with SingleTickerProviderStateMixin {
+
+  late final TabController controller;
+  int index = 0;
+  int selectedTab = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+    controller.addListener(tabListener);
+  }
+
+  void tabListener() {
+    setState(() {
+      index = controller.index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: const MainAppBar(),
       body: renderBody(),
     );
   }
 
   Widget renderBody() {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          renderTabBar()
+        ],
+      ),
+    );
   }
+  
+  Widget renderTabBar() {
+    const TextStyle selectedTextStyle = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+    );
+
+    const TextStyle unSelectedTextStyle = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+    );
+
+    return SizedBox(
+      height: 38,
+      child: TabBar(
+        controller: controller,
+        isScrollable: true,
+        indicatorColor: purple,
+        indicatorWeight: 3.0,
+        indicatorPadding: const EdgeInsets.only(left: 10, right: 10),
+        labelColor: purple,
+        labelStyle: selectedTextStyle,
+        unselectedLabelColor: Colors.grey,
+        unselectedLabelStyle: unSelectedTextStyle,
+        tabs: [
+          renderTabItem('회의실'),
+          renderTabItem('차량'),
+          renderTabItem('차량')
+        ]
+      ),
+    );
+  }
+
+  Widget renderTabItem(String title) {
+    return SizedBox(
+      width: (MediaQuery.of(context).size.width - 100) / 3 ,
+      child: Tab(icon: Text(title),),
+    );
+  }
+  
+  
 }
