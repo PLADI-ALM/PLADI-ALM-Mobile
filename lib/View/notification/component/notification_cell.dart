5f3a4683b7e64cc6d3f1a31ac0871e8101ae6c66
chayoosang <yoo7969@naver.com>
2023-11-06 16:18:22 +0900

    [PDA-11] feat: 알람 cell 생성

--- lib/View/notification/component/notification_cell.dart
+++ lib/View/notification/component/notification_cell.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+enum NotificationType {
+  Request('asset/image/clock_notification.png', "장비 예약 요청"),
+  Approve('asset/image/check_notification.png', "장비 예약 승인"),
+  Cancel('asset/image/cancel_notification.png', "장비 예약 반려"),
+  Return('asset/image/check_notification.png', "장비 예약 반납"),
+  End('asset/image/time_notification.png', "장비 예약 종료");
+
+  const NotificationType(this.image, this.title);
+
+  final String image;
+  final String title;
+}
 
 class NotificationCell extends StatefulWidget {
   const NotificationCell({super.key});
@@ -10,6 +24,13 @@ class NotificationCell extends StatefulWidget {
 class _NotificationCellState extends State<NotificationCell> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        SvgPicture.asset('asset/image/time_notification'),
+        const Column(
+          children: [Text("장비 예약 요청"), Text("차루비님이 맥북 예약을 요청하셨습니다.")],
+        )
+      ],
+    );
   }
 }
